7ee4b1bf6b5cbd491dd4425e293f8267cdbec51c
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-07-04 23:18:55 +0300

    new solution

--- solution.py
+++ solution.py
@@ -5,8 +5,5 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        allnums = set([i for i in range(1, n+1)])
-        nums = set(nums)
-        return list(allnums.difference(nums))
-
-print(Solution().findDisappearedNumbers([1,1]))
+        allnums = [i for i in range(1, n+1) if i not in nums]
+        return allnums
